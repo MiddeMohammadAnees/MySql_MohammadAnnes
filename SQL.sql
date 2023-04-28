5d5525056d44969f5ced6ea85c3f66ae5e0c5590
MiddeMohammadAnees <130051750+MiddeMohammadAnees@users.noreply.github.com>
2023-04-28 10:25:55 +0530

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,22 @@
+-- create database HeroViredRgm;
+-- create table RGMCET(Student_id int primary key not null,Student_name varchar(20) not null,Student_College varchar(20) not null,SessionOn varchar(90) not null,Address varchar(90) not null, Contact int not null);
+-- INSERT INTO RGMCET(Student_id,Student_name,Student_College,SessionOn,Address,Contact)
+-- VALUES(77,"Anees","RGM","MySql","INDIA",92326368);
+-- select * from RGMCET
+-- INSERT INTO RGMCET VALUES(101,"Mohammad","RGM","Python","India",12345678);
+INSERT INTO RGMCET VALUES(102,"Rohith","RGM","C++","India",12345678);
+INSERT INTO RGMCET VALUES(103,"Javid","RGM","Python","India",12345678);
+INSERT INTO RGMCET VALUES(104,"Shiva","RGM","Python","India",12345678);
+INSERT INTO RGMCET VALUES(105,"Sahil","RGM","Java","India",12345678);
+INSERT INTO RGMCET VALUES(106,"Shoaib","RGM","Python","India",12345678);
+INSERT INTO RGMCET VALUES(107,"Sai","RGM","MySql","India",12345678);
+INSERT INTO RGMCET VALUES(108,"Pavan","RGM","C","India",12345678);
+INSERT INTO RGMCET VALUES(109,"Muzafar","RGM","Java","India",12345678);
+INSERT INTO RGMCET VALUES(111,"MuniKishore","RGM","Python","India",12345678);
+INSERT INTO RGMCET VALUES(112,"Macha","RGM","MySql","India",12345678);
+INSERT INTO RGMCET VALUES(113,"Ranga","RGM","C++","India",12345678);
+INSERT INTO RGMCET VALUES(114,"Obul","RGM","Python","India",12345678);
+INSERT INTO RGMCET VALUES(115,"Bargav","RGM","Python","India",12345678);
+INSERT INTO RGMCET VALUES(116,"Nagaraju","RGM","Java","India",12345678);
+INSERT INTO RGMCET VALUES(118,"Manoj","RGM","Oracle","India",12345678);
+select * from RGMCET
